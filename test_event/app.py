f2ecb95c71cb4d420828a43a18db46f3c12ac8f0
korsh624 <korsh624@gmail.com>
2022-04-22 08:52:16 +0300

    aaa

--- test_event/app.py
+++ test_event/app.py
@@ -2,14 +2,21 @@
 from flask import Flask, render_template, Response, request, redirect, url_for
 servo=90
 app = Flask(__name__)
-@app.route('/json')
+@app.route('/')
 def json():
     return render_template('json.html',servo=servo)
 #background process happening without any refreshing
-@app.route('/background_process_test')
-def background_process_test():
+@app.route('/forvard')
+def forvard():
     global servo
     servo=servo+1
+    print(servo)
+    return ("nothing")
+
+@app.route('/resvard')
+def resvard():
+    global servo
+    servo=servo-1
     print(servo)
     return ("nothing")
 if __name__ == '__main__':
